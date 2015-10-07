64e3c43c1cccbfb2d9b872b5c19e7dbcfe9f3b62
Rufus Mbugua <mbuguarufus@gmail.com>
2015-10-07 08:32:44 +0300

    Compiling Config
    Signed-off-by: Rufus Mbugua <mbuguarufus@gmail.com>

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -12,6 +12,7 @@ return array(
     'App\\Console\\Kernel' => $baseDir . '/app/Console/Kernel.php',
     'App\\Events\\Event' => $baseDir . '/app/Events/Event.php',
     'App\\Exceptions\\Handler' => $baseDir . '/app/Exceptions/Handler.php',
+    'App\\Http\\Controllers\\AdminController' => $baseDir . '/app/Http/Controllers/AdminController.php',
     'App\\Http\\Controllers\\Auth\\AuthController' => $baseDir . '/app/Http/Controllers/Auth/AuthController.php',
     'App\\Http\\Controllers\\Auth\\PasswordController' => $baseDir . '/app/Http/Controllers/Auth/PasswordController.php',
     'App\\Http\\Controllers\\Controller' => $baseDir . '/app/Http/Controllers/Controller.php',
