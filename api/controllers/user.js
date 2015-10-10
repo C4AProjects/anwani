2009dd602945c7a0a5c03a6f2ac3fb48739af6e0
Tony Mutai <tonimut7@gmail.com>
2015-10-10 17:56:03 +0300

    add password update feature

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -8,6 +8,7 @@ var debug  = require('debug')('anwani-api:user-controller');
 var async  = require('async');
 var moment = require('moment');
 var _     = require('lodash');
+var emquery = require('emquery');
 
 var Address   = require('../dal/address');
 var User      = require('../dal/user');
@@ -15,6 +16,7 @@ var Token     = require('../dal/token');
 var UserModel = require('../models/user');
 var config    = require('../config');
 var CustomError = require('../lib/custom-error');
+var hashSecurityAnswer = require('../lib/utils').hashSecurityAnswer;
 
 /**
  * Create a user.
@@ -39,7 +41,7 @@ exports.create = function createUser(req, res, next) {
 
     if(errs) {
       return next(CustomError({
-        name: 'ADDRESS_CREATION_ERROR',
+        name: 'USER_CREATION_ERROR',
         message: errs.message
       }));
     }
@@ -52,8 +54,8 @@ exports.create = function createUser(req, res, next) {
 
     User.create(body, function (err, user) {
       if(err) {
-        return next(customeerror({
-          name: 'user_creation_error',
+        return next(CustomError({
+          name: 'USER_CREATION_ERROR',
           message: err.message
         }));
       }
@@ -129,49 +131,24 @@ exports.update = function updateUser(req, res, next) {
     _id: req.params.id
   };
   var body = req.body;
-  var update;
+  var data;
 
-  if(body.password) {
-    User.hashPasswd(body.password, function (err, hash) {
+  data = {
+    $set: emquery(body)
+  };
 
-      body.password = hash;
+  User.update(query, data, function cb(err, user) {
+    if(err) {
+      return next(CustomError({
+        name: 'SERVER_ERROR',
+        message: err.message,
+        status: 500
+      }));
+    }
 
-      update = {
-        $set: body
-      };
+    res.json(user || {});
 
-      User.update(query, update, function cb(err, user) {
-        if(err) {
-          return next(CustomError({
-            name: 'SERVER_ERROR',
-            message: err.message,
-            status: 500
-          }));
-        }
-
-        res.json(user || {});
-
-      });
-    });
-  } else {
-
-    update = {
-      $set: body
-    };
-
-    User.update(query, update, function cb(err, user) {
-      if(err) {
-        return next(CustomError({
-          name: 'SERVER_ERROR',
-          message: err.message,
-          status: 500
-        }));
-      }
-
-      res.json(user || {});
-
-    });
-  }
+  });
 
 };
 
@@ -252,7 +229,7 @@ exports.delete = function deleteUser(req, res, next) {
 exports.fetchAll = function fetchAllUsers(req, res, next) {
   debug('get a collection of users');
 
-  if(!req._user || (req._user.realm !== 'admin')) {
+  if(!req._user || (req._user.role !== 'admin')) {
     return next(CustomError({
       name: 'AUTHORIZATION_ERROR',
       message: 'Access Denied, only admins allowed'
@@ -314,5 +291,123 @@ exports.fetchUserAddresses = function fetchAllUserAddresses(req, res, next) {
     }
 
     res.json(addresses);
+  });
+};
+
+/**
+ * Update the password of a user
+ *
+ * @desc
+ *
+ * @param {Object} req HTTP Request Object
+ * @param {Object} res HTTP Response Object
+ * @param {Function} next Middleware dispatcher
+ */
+exports.updatePassword = function updatePassword(req, res, next) {
+  debug('updating password for ' + req.body.phone_number);
+
+  var body = req.body;
+
+  if(!body.phone_number && !body.security_answer && !body.new_password) {
+    return next(CustomError({
+      name: 'PASSWORD_UPDATE_ERROR',
+      message: 'Phone number, New password/pin and Security Question Answer not provided'
+    }));
+  }
+
+  var query = {
+    phone_number: body.phone_number
+  };
+
+  async.waterfall([
+    function findUser(done) {
+      UserModel.findOne(query, function (err, user) {
+        if(err) {
+          return done(CustomError({
+            name: 'SERVER_ERROR',
+            message: err.message,
+            status: 500
+          }));
+        }
+
+        if(!user._id) {
+          return done(CustomError({
+            name: 'PASSWORD_UPDATE_ERROR',
+            message: 'Id(' + id + ') is not recognized'
+          }));
+
+        }
+
+        done(null, user);
+      });
+    },
+    function verifySecurityQuestion(user, done) {
+      hashSecurityAnswer(body.security_answer, function (err, hashed) {
+        if(err) {
+          return done(CustomError({
+            name: 'SERVER_ERROR',
+            message: err.message,
+            status: 500
+          }));
+        }
+
+        if(user.security_pass.answer !== hashed) {
+          return done(CustomError({
+            name: 'PASSWORD_UPDATE_ERROR',
+            message: 'Security question answer dont match'
+          }));
+        } else {
+
+          return done(null, user);
+        }
+      });
+    },
+    function hashNewPassword(user, done) {
+      User.hashPasswd(body.new_password, function (err, hash) {
+        if(err) {
+          return done(CustomError({
+            name: 'SERVER_ERROR',
+            message: err.message,
+            status: 500
+          }));
+        }
+
+        var update = {
+          password: hash
+        };
+
+        done(null, update, user);
+      });
+    },
+    function updateUserPassword(update, user, done) {
+      query = {
+        _id: user._id
+      };
+
+      User.update(query, update, function cb(err, user) {
+        if(err) {
+          return done(CustomError({
+            name: 'SERVER_ERROR',
+            message: err.message,
+            status: 500
+          }));
+        }
+
+
+        done(null, { updated: true });
+
+      });
+    }
+  ], function (err, results) {
+    if(err) {
+      return next(CustomError({
+        name: 'SERVER_ERROR',
+        message: err.message,
+        status: 500
+      }));
+    }
+
+    res.json(results);
+
   });
 };
