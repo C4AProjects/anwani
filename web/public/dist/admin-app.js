b53f12834c1c6c4c36c1337640c998e12493cc52
Rufus Mbugua <mbuguarufus@gmail.com>
2015-10-11 20:04:50 +0300

    Modified Main JS
    Signed-off-by: Rufus Mbugua <mbuguarufus@gmail.com>

--- web/public/dist/admin-app.js
+++ web/public/dist/admin-app.js
@@ -2240,29 +2240,29 @@ app.controller('FormValidationCtrl', ['$scope', function($scope) {
 /* Controllers */
 // signin controller
 app.controller('LoginFormController', ['$scope', '$http', '$state', 'localStorageService', '$rootScope',
-  function (scope, http, state, localStorageService, rootScope) {
-    scope.user = {};
-    scope.authError = null;
-    scope.login = function () {
-      scope.authError = null;
-      // Try to login
-      http.post('http://anwaniapi.mybluemix.net/users/login', scope.user)
-          .then(function (response) {
-            console.log(response.data);
-            if (!response.data.user) {
-              scope.authError = 'Phone Number or Password not right';
-            } else {
-              rootScope.user = response.user;
-              localStorageService.set('user', response.user);
-              rootScope.token = response.token;
-              localStorageService.set('token', response.token);
-              state.go('app.dashboard');
-            }
-          }, function (x) {
-            scope.authError = 'Server Error';
-          });
+    function (scope, http, state, localStorageService, rootScope) {
+        scope.user = {};
+        scope.authError = null;
+        scope.login = function () {
+            scope.authError = null;
+            // Try to login
+            http.post('http://anwaniapi.mybluemix.net/users/login', scope.user)
+                .then(function (response) {
+                    console.log(response.data);
+                    if (!response.data.user) {
+                        scope.authError = 'Phone Number or Password not right';
+                    } else {
+                        rootScope.user = response.user;
+                        localStorageService.set('user', response.user);
+                        rootScope.token = response.token;
+                        localStorageService.set('token', response.token);
+                        state.go('app.dashboard');
+                    }
+                }, function (x) {
+                    scope.authError = 'Server Error';
+                });
     };
-  }])
+    }])
 ;;app.controller('MailCtrl', ['$scope', function($scope) {
   $scope.folds = [{
     name: 'Inbox',
@@ -2450,22 +2450,22 @@ app.controller('NotifyCtrl', function($scope,notify){
 
 // signup controller
 app.controller('RegisterFormController', ['$scope', '$http', '$state', 'localStorageService', '$rootScope',
-  function (scope, http, state, localStorageService, rootScope) {
-    scope.user = {};
-    scope.authError = null;
-    scope.register = function () {
-      scope.authError = null;
+    function (scope, http, state, localStorageService, rootScope) {
+        scope.user = {};
+        scope.authError = null;
+        scope.register = function () {
+            scope.authError = null;
       // Try to create
-      http.post('http://anwaniapi.mybluemix.net/users/signup', scope.user)
+            http.post('http://anwaniapi.mybluemix.net/users/signup', scope.user)
       .then(function(response) {
         if ( !response.data.user ) {
-          scope.authError = response;
+            scope.authError = response;
         }else{
-          rootScope.newUser = response.user;
-          state.go('app.dashboard');
+            rootScope.newUser = response.user;
+            state.go('app.dashboard');
         }
       }, function(x) {
-            scope.authError = 'Server Error';
+                    scope.authError = 'Server Error';
       });
     };
   }])
