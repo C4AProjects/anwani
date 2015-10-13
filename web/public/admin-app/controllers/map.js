9108003ff306675a65825ab26e35bb756b0cea50
Rufus Mbugua <mbuguarufus@gmail.com>
2015-10-13 14:28:55 +0300

    Preparing for API Calls
    Signed-off-by: Rufus Mbugua <mbuguarufus@gmail.com>

--- web/public/admin-app/controllers/map.js
+++ web/public/admin-app/controllers/map.js
@@ -1,5 +1,12 @@
 app.controller('MapsCtrl',['$scope',function(scope){
-    scope.map = { center: { latitude: -1.2833, longitude: 36.8167 }, zoom: 7 };
+    scope.map = {
+        center:
+        {
+            latitude: -1.2833,
+            longitude: 36.8167
+        },
+        zoom: 7
+    };
 }]);
 
 
--- web/public/dist/admin-app.js
+++ web/public/dist/admin-app.js
@@ -2,85 +2,115 @@
 
 // Declare app level module which depends on views, and components
 var app = angular.module('admin', [
-  'ngAnimate',
-  //    'ngCookies',
-  //    'ngResource',
-  //    'ngSanitize',
-  'ngTouch',
-  //    'ngStorage',
-  'ui.router',
-  'ui.bootstrap',
-  // 'ui.utils',
-  'ui.load',
-  'ui.jq',
-  'oc.lazyLoad',
-  'perfect_scrollbar',
-  'angular-inview',
-  'angular-loading-bar',
-  'LocalStorageModule',
-  'smart-table'
+    'ngAnimate',
+    //    'ngCookies',
+    //    'ngResource',
+    //    'ngSanitize',
+    'ngTouch',
+    //    'ngStorage',
+    'ui.router',
+    'ui.bootstrap',
+    // 'ui.utils',
+    'ui.load',
+    'ui.jq',
+    'oc.lazyLoad',
+    'perfect_scrollbar',
+    'angular-inview',
+    'angular-loading-bar',
+    'LocalStorageModule',
+    'smart-table',
+    'permission',
+    'uiGmapgoogle-maps'
 ]);
 
 app.config(
     ['$controllerProvider', '$compileProvider', '$filterProvider', '$provide',
-      function ($controllerProvider, $compileProvider, $filterProvider, $provide) {
+        function ($controllerProvider, $compileProvider, $filterProvider, $provide) {
 
-        // lazy controller, directive and service
-        app.controller = $controllerProvider.register;
-        app.directive = $compileProvider.directive;
-        app.filter = $filterProvider.register;
-        app.factory = $provide.factory;
-        app.service = $provide.service;
-        app.constant = $provide.constant;
-        app.value = $provide.value;
-      }
+            // lazy controller, directive and service
+            app.controller = $controllerProvider.register;
+            app.directive = $compileProvider.directive;
+            app.filter = $filterProvider.register;
+            app.factory = $provide.factory;
+            app.service = $provide.service;
+            app.constant = $provide.constant;
+            app.value = $provide.value;
+        }
     ]);
 
 app.run(
     ['localStorageService', '$rootScope','$http','$state',
-      function (localStorageService, rootScope,http,state) {
-        var user = localStorageService.get('user');
-        var token = localStorageService.get('token');
-        rootScope.addresses = [{
-          "_id" : "556e1174a8952c9521286a60",
-          user: "556e1174a8952c9521286a60",
-          short_virtual_code: "MP7H+E2",
-          long_virtual_code: "6EAEMMP7H+E2",
-          location_pic: "/media/a8952c9521286a60.jpeg",
-          latitude: 4.567889,
-          longitude: -12.098,
-          street_address: "",
-          city: "nairobi",
-          country: "kenya"
-        },
-          {
-            "_id" : "556e1174a8952c9521286a60",
-            user: "556e1174a8952c9521286a60",
-            short_virtual_code: "MP7H+E2",
-            long_virtual_code: "6EAEMMP7H+E2",
-            location_pic: "/media/a8952c9521286a60.jpeg",
-            latitude: 4.567889,
-            longitude: -12.098,
-            street_address: "",
-            city: "nakuru",
-            country: "kenya"
-          }];
-        rootScope.addresses_shared = [];
-        if (user && token) {
-          rootScope.user = user;
-          rootScope.token = token;
-          http.defaults.headers.post = { 'Authorization' : 'Bearer '+localStorageService.get('token') };
-          http.defaults.headers.get = { 'Authorization' : 'Bearer '+localStorageService.get('token') }
-        }
+        function (localStorageService, rootScope,http,state) {
 
-        rootScope.logout = function logout()
-        {
-          localStorageService.remove('user');
-          localStorageService.remove('token');
-          state.go('login');
 
+            rootScope.users=[];
+            var user = localStorageService.get('user');
+            var token = localStorageService.get('token');
+            rootScope.subscriber = false;
+            rootScope.addresses = [{
+                "_id" : "556e1174a8952c9521286a60",
+                user: "556e1174a8952c9521286a60",
+                short_virtual_code: "MP7H+E2",
+                long_virtual_code: "6EAEMMP7H+E2",
+                location_pic: "/media/a8952c9521286a60.jpeg",
+                latitude: -0.3000,
+                longitude: 36.0667,
+                street_address: "",
+                city: "nairobi",
+                country: "kenya"
+            },
+                {
+                    "_id" : "556e1174a8952c9521286a60",
+                    user: "556e1174a8952c9521286a60",
+                    short_virtual_code: "MP7H+E2",
+                    long_virtual_code: "6EAEMMP7H+E2",
+                    location_pic: "/media/a8952c9521286a60.jpeg",
+                    latitude: -0.7202,
+                    longitude: 36.4285,
+                    street_address: "",
+                    city: "nakuru",
+                    country: "kenya"
+                }];
+            rootScope.addresses_shared = [];
+            if (user && token) {
+                rootScope.user = user;
+                rootScope.token = token;
+                http.defaults.headers.post = { 'Authorization' : 'Bearer '+localStorageService.get('token') };
+                http.defaults.headers.get = { 'Authorization' : 'Bearer '+localStorageService.get('token') }
+            }
+
+            rootScope.logout = function logout()
+            {
+                localStorageService.remove('user');
+                localStorageService.remove('token');
+                state.go('login');
+
+            };
+            //rootScope.user={
+            //    "name":"ZUKU",
+            //    "website":"www.zuku.co.ke",
+            //    "address":"Nairobi",
+            //    "email":"info@zuku.co.ke",
+            //    "logo":"http://vividfeatures.com/wp-content/uploads/2013/08/zuku.jpg",
+            //    "role":"subscriber"
+            //};
+          if(rootScope.user) {
+            if (rootScope.user.role) {
+              if (rootScope.user.role = "subscriber") {
+                rootScope.subscriber = true;
+              }
+            }
+          }
+
+
+            rootScope.state = state;
+            //console.log(rootScope.state.$current);
+            //if(rootScope.state.$current.url.source.search('/new')<0
+            //        &&
+            //        rootScope.user.role!="admin"){
+            //  state.go('login');
+            //}
         }
-      }
     ]);;// lazyload config
 
 app.constant('JQ_CONFIG', {
@@ -2302,10 +2332,10 @@ app.controller('LoginFormController', ['$scope', '$http', '$state', 'localStorag
         scope.login = function () {
             scope.authError = null;
             // Try to login
-            http.post('http://anwaniapi.mybluemix.net/users/login', scope.user)
+            http.post('http://anwaniapi.mybluemix.net/subscribers/login', scope.user)
                 .then(function (response) {
                     if (!response.data.user) {
-                        scope.authError = 'Phone Number or Password not right';
+                        scope.authError = 'Email or Password not right';
                     } else {
                         rootScope.user = response.data.user;
                         localStorageService.set('user', response.data.user);
@@ -2434,6 +2464,18 @@ app.directive('labelColor', function() {
     });
   }
 });
+;app.controller('MapsCtrl',['$scope',function(scope){
+    scope.map = {
+        center:
+        {
+            latitude: -1.2833,
+            longitude: 36.8167
+        },
+        zoom: 7
+    };
+}]);
+
+
 ;app.controller('MembersCtrl', ['$scope', '$http',
   function ($scope, $http) {
     $http.get('data/members.json').success(function(data) {
@@ -2508,6 +2550,10 @@ app.controller('RegisterFormController', ['$scope', '$http', '$state', 'localSto
     function (scope, http, state, localStorageService, rootScope) {
         scope.user = {};
         scope.authError = null;
+
+        /**
+         * @description Register Regular User
+         */
         scope.register = function () {
             scope.authError = null;
             // Try to create
@@ -2523,6 +2569,24 @@ app.controller('RegisterFormController', ['$scope', '$http', '$state', 'localSto
                     scope.authError = 'Server Error';
                 });
         };
+        /**
+         * @description Function for Registering a Subsriber
+         *
+         */
+        scope.registerSubscriber = function registerSubscriber(){
+
+            http.post('http://anwaniapi.mybluemix.net/subscribers/signup', scope.user)
+                .then(function(response) {
+                    if (!response.data) {
+                        scope.authError = response;
+                    }else{
+                        rootScope.newUser = response.data;
+                        state.go('app.dashboard');
+                    }
+                }, function(x) {
+                    scope.authError = 'Server Error';
+                });
+        }
     }])
 ;;/**
  */
@@ -4687,7 +4751,7 @@ app.controller('MapCtrl', ['$scope', function ($scope) {
      * @param  {[type]} "/account/expenditure" [description]
      * @return {[type]}                        [description]
      */
-    $urlRouterProvider.otherwise("/dashboard");
+    $urlRouterProvider.otherwise("/login");
 
     // Now set up the states
     /**
@@ -4749,6 +4813,19 @@ app.controller('MapCtrl', ['$scope', function ($scope) {
         .state('app.address.view', {
           url: '/view',
           templateUrl: '../admin-app/partials/address-view.html'
+        })
+        .state('app.users', {
+          url: '/users',
+          templateUrl: '../admin-app/partials/users.html'
+        })
+        .state('app.users.view', {
+          url: '/view',
+          templateUrl: '../admin-app/partials/users-view.html'
+        })
+        .state('app.map', {
+          url: '/map',
+          controller:"MapsCtrl",
+          templateUrl: '../admin-app/partials/ui-map.html'
         })
     ;
   }
