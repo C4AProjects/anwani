5c3fa0bcf3822a3547bbb1f8184a2f182ac93daa
Rufus Mbugua <mbuguarufus@gmail.com>
2015-11-18 12:34:27 +0300

    Changed Social Icons to symbols

--- web/tmp/partials.js
+++ web/tmp/partials.js
@@ -1,33 +1,4 @@
-angular.module('templates-dist', ['../public/app/partials/account/index.html', '../public/app/partials/account/login.html', '../public/app/partials/home/about.html', '../public/app/partials/home/banner.html', '../public/app/partials/home/features.html', '../public/app/partials/home/footer.html', '../public/app/partials/home/header.html', '../public/app/partials/home/index.html', '../public/app/partials/home/partners.html', '../public/app/partials/home/sub-header.html', '../public/app/partials/test/index.html']);
-
-angular.module("../public/app/partials/account/index.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("../public/app/partials/account/index.html",
-    "");
-}]);
-
-angular.module("../public/app/partials/account/login.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("../public/app/partials/account/login.html",
-    "<div class=\"full content\">\n" +
-    "  <form action=\"\" class=\"ui form\" class=\"left-form\" id=\"login\">\n" +
-    "    <h3 class=\"ui header\">\n" +
-    "      Login\n" +
-    "    </h3>\n" +
-    "    <div class=\"inner\">\n" +
-    "    <div class=\"ui input\">\n" +
-    "      <label for=\"\">Username</label>\n" +
-    "      <input type=\"text\" placeholder=\"John Doe\">\n" +
-    "    </div>\n" +
-    "    <div class=\"ui input\">\n" +
-    "      <label for=\"\">Password</label>\n" +
-    "      <input type=\"password\" placeholder=\"Enter Password Here...\">\n" +
-    "    </div>\n" +
-    "    <div class=\"ui green button\">Login</div>\n" +
-    "      </div>\n" +
-    "\n" +
-    "  </form>\n" +
-    "</div>\n" +
-    "");
-}]);
+angular.module('templates-dist', ['../public/app/partials/home/about.html', '../public/app/partials/home/banner.html', '../public/app/partials/home/features.html', '../public/app/partials/home/footer.html', '../public/app/partials/home/header.html', '../public/app/partials/home/index.html', '../public/app/partials/home/partners.html', '../public/app/partials/home/sub-header.html']);
 
 angular.module("../public/app/partials/home/about.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("../public/app/partials/home/about.html",
@@ -37,8 +8,8 @@ angular.module("../public/app/partials/home/about.html", []).run(["$templateCach
     "    <div class=\"col-md-12\">\n" +
     "      <div class=\"col-md-4 slighted-above centered\">\n" +
     "        <span class=\"black-text\" translate>about.first.share</span>\n" +
-    "        <a class=\"symbol\" title='circlefacebook' href=\"https://www.facebook.com/Anwani-937650086328395/\"></a>\n" +
-    "        <a class=\"symbol\" title='circletwitterbird' href=\"https://twitter.com/appanwani\"></a>\n" +
+    "        <a class=\"symbol facebook\" href=\"https://www.facebook.com/Anwani-937650086328395/\">&#xe227;</a>\n" +
+    "        <a class=\"symbol twitter\" href=\"https://twitter.com/appanwani\">&#xe286;</a>\n" +
     "      </div>\n" +
     "      <div class=\"col-md-offset-8 centered col-md-4 slighted-above\">\n" +
     "        <span style=\"display:block\" class='black-text' translate>about.first.featured</span>\n" +
@@ -211,8 +182,8 @@ angular.module("../public/app/partials/home/footer.html", []).run(["$templateCac
     "<footer class=\"row white\">\n" +
     "  <div class=\"col-md-4 centered\">\n" +
     "    <span class=\"block gray-text\" translate>footer.col_1.description</span>\n" +
-    "    <a class=\"symbol\" title='circlefacebook' href=\"https://www.facebook.com/Anwani-937650086328395/\"></a>\n" +
-    "    <a class=\"symbol\" title='circletwitterbird' href=\"https://twitter.com/appanwani\"></a>\n" +
+    "    <a class=\"symbol facebook\" href=\"https://www.facebook.com/Anwani-937650086328395/\">&#xe227;</a>\n" +
+    "    <a class=\"symbol twitter\" href=\"https://twitter.com/appanwani\">&#xe286;</a>\n" +
     "  </div>\n" +
     "  <div class=\"col-md-4 centered right\">\n" +
     "    <span class=\"gray-text\" translate>footer.col_2.description</span>\n" +
@@ -311,11 +282,5 @@ angular.module("../public/app/partials/home/sub-header.html", []).run(["$templat
     "    </ul>\n" +
     "  </nav>\n" +
     "</div>\n" +
-    "");
-}]);
-
-angular.module("../public/app/partials/test/index.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("../public/app/partials/test/index.html",
-    "<h1>Test Page</h1>\n" +
     "");
 }]);
